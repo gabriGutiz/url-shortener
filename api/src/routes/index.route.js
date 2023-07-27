162b21c7301e11246cd595a9f5dc417c4bdd60ec
Gabriel Gutierrez <gabri.gutiz@gmail.com>
2023-07-27 15:36:56 -0300

    alterando retorno NotFound para url encurtada

--- api/src/routes/index.route.js
+++ api/src/routes/index.route.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { CustomError } from '../utils/CustomError.js';
 import { IndexService } from '../services/index.service.js';
+import { retornoNotFound } from '../utils/notFound.util.js';
 
 const router = express.Router();
 
@@ -15,7 +16,7 @@ router.get('/:id', async (req, res, next) => {
         })
         .catch((err) => {
             if (err instanceof CustomError && err.statusCode === 404) {
-                return res.sendFile('/src/NotFound.html');
+                return res.status(404).send(retornoNotFound);
             }
             next(err)
         });
